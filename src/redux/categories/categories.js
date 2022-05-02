edf602f3b1f4d1d11b9a13ed5df5e6d286bdffc4
kosher <lionel.kossi@gmail.com>
2022-05-02 23:36:59 +0000

    Remove unecessary files

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,19 +1,7 @@
-const ADD = 'my-app/categories/ADD';
-const REMOVE = 'my-app/categories/REMOVE';
 const CHECK_STATUS = 'my-app/categories/CHECK_STATUS';
 
 export default function categoryReducer(state = [], action) {
   switch (action.type) {
-    case ADD:
-      return [
-        ...state,
-        {
-          id: action.id,
-        },
-      ];
-    case REMOVE:
-      return state.filter((category) => category.id !== action.id);
-
     case CHECK_STATUS:
       return state.map((category) => {
         if (category.id === action.id) {
@@ -27,20 +15,6 @@ export default function categoryReducer(state = [], action) {
     default:
       return state;
   }
-}
-
-export function addCategory(category) {
-  return {
-    type: ADD,
-    category,
-  };
-}
-
-export function remove(id) {
-  return {
-    type: REMOVE,
-    id,
-  };
 }
 
 export function checkStatus() {
