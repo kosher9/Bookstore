be2224685038ce423a0d5dfd8d8da9dcb1397227
kosher <lionel.kossi@gmail.com>
2022-05-02 22:21:51 +0000

    Write book actions

---
+++ src/redux/books/books.js
@@ -0,0 +1,32 @@
+const ADD = 'my-app/books/ADD';
+const REMOVE = 'my-app/books/REMOVE';
+
+export default function reducer(state = [], action) {
+  switch (action.type) {
+    case ADD:
+      return [
+        ...state,
+        {
+          id: action.id,
+        },
+      ];
+    case REMOVE:
+      return state.filter((book) => book.id !== action.id);
+    default:
+      return state;
+  }
+}
+
+export function addBook(id) {
+  return {
+    type: ADD,
+    id,
+  };
+}
+
+export function remove(id) {
+  return {
+    type: REMOVE,
+    id,
+  };
+}
