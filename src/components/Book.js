51fb114ab5d1a000e3fce2b86936e5ccd7ce0a08
kosher <lionel.kossi@gmail.com>
2022-05-07 00:05:47 +0000

    Add Nav bar and Book css files

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Book.css';
 import { useDispatch } from 'react-redux';
 import { deleteBook } from '../redux/books/books';
 
@@ -12,6 +13,35 @@ export default function Book(props) {
 
   return (
     <div className="book">
+      <div className="ctn-left">
+        <div className="ctn-book-info">
+          <h5 className="categpry">{book.category}</h5>
+          <h2 className="title">{book.title}</h2>
+          <h5 className="author">{book.author}</h5>
+        </div>
+        <div className="ctn-book-action">
+          <h4>Comments</h4>
+          <h4>Remove</h4>
+          <h4>Edit</h4>
+        </div>
+      </div>
+
+      <div className="ctn-middle">
+        <div>
+          <image src={book.image} alt={book.title} />
+        </div>
+        <div className="state-progress">
+          <h5>64%</h5>
+          <h5>completed</h5>
+        </div>
+      </div>
+
+      <div className="ctn-right">
+        <h5>CURRENT CHAPTER</h5>
+        <h5>Chapter 14</h5>
+        <button type="button">UPDATE PROGRESS</button>
+      </div>
+
       <div className="book-title">{book.title}</div>
       <div className="book-authors">{book.author}</div>
       <button type="button" onClick={removeBook}>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import './NavBar.css';
 import { Link } from 'react-router-dom';
 
 export default function NavBar() {
   return (
-    <nav>
-      <Link to="/">Books</Link>
-      <Link to="/categories">Categories</Link>
-    </nav>
+    <header>
+      <h3>Bookstore CMS</h3>
+      <nav>
+        <Link to="/">Books</Link>
+        <Link to="/categories">Categories</Link>
+      </nav>
+    </header>
   );
 }
